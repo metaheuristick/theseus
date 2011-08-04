a1604b14325a63165cc4c8d27f5d9008ebb59261
Robert Teto <robert.teto@gmail.com>
2011-08-03 23:33:39 -0400

    Let's do this.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theseus::Application.config.secret_token = '5c9deb1fe440bd4fbf79d206c9dcf3ac75b4ed31791441042ea908a4e2d38e814ad6a1f42af956c2f3d26f9bad14c321107a925df20f3f14ff4ebc3bf22e75a3'
